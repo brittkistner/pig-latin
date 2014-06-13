7c92749f30dfbe40907fbeb8dd45ca03d22e7ba2
Brittney Kistner <bkistner01@gmail.com>
2014-06-13 14:40:55 -0700

    Completed PigLatin::Translator tests

---
+++ lib/pig_latin.rb
@@ -0,0 +1,16 @@
+require "pig_latin/0.0.1"
+
+module PigLatin
+
+  module Translator
+    def self.
+    end
+
+    def self.collect_word(word)
+    end
+
+    def self.translate_word(word)
+      /\A[^aeiou]+/
+    end
+  end
+end
---
+++ spec/pig_latin_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'PigLatin' do
+
+  it "exists" do
+    expect(PigLatin).to be_a(Module)
+  end
+end
+
+describe 'Translator' do
+  it "exists" do
+    expect(PigLatin::Translator).to be_a(Module)
+  end
+
+  describe '.translate_word' do
+    it "translates a word with a consonant as the first letter to pig latin" do
+      expect(Translator.translate_word("hello")).to eq("ellohay")
+    end
+
+    it "translates a word with a vowel as the first letter to pig latin" do
+      expect(Translator.translate_word("orange")).to eq("orangeway")
+    end
+
+    it "translates a word with 'y' as the first letter to pig latin" do
+      expect(Translator.translate_word("yay")).to eq("ayyay")
+    end
+
+end
+
+
